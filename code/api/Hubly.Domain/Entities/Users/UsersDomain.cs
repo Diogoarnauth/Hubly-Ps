a02f7661bdaa4281f45d78daa4297f70b884cd0d
Carolina_Coelho <161573493+carolina-coelho@users.noreply.github.com>
2026-03-12 19:07:10 +0000

    Correção do erro changePassword

--- code/api/Hubly.Domain/Entities/Users/UsersDomain.cs
+++ code/api/Hubly.Domain/Entities/Users/UsersDomain.cs
@@ -22,17 +22,18 @@ public class UsersDomain
     }
 
     // Criar o Hash da password (equivalente ao passwordEncoder.encode)
+    /*
     public PasswordValidationInfo CreatePasswordValidationInformation(string password)
     {
         string hash = BCrypt.Net.BCrypt.HashPassword(password);
         return new PasswordValidationInfo(hash);
-    }
+    }*/
 
     // Validar se a password bate certo com o hash (equivalente ao matches)
-    public bool ValidatePassword(string password, PasswordValidationInfo info)
+    /*public bool ValidatePassword(string password, PasswordValidationInfo info)
     {
         return BCrypt.Net.BCrypt.Verify(password, info.ValidationInfo);
-    }
+    }*/
 /*
     public string GenerateTokenValue()
     {
